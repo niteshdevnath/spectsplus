c004f03395ab10db99dd850941902ba2f05c3880
Nitesh devnath <nitesh@sjainventures.com>
2018-12-15 15:59:08 +0530

    fsdf

--- application/views/web/product_details.php
+++ application/views/web/product_details.php
@@ -1,10 +1,6 @@
 <?php
 		echo	$layout['header'];
 ?>
-<link src="<?php echo base_url('assets/css/jqzoom.css'); ?>"  type="text/css" role="stylesheet" />
-<link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
-<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script></div>
-<script type="text/javascript" src="<?php echo base_url('assets/js/jqzoom.js'); ?>"></script>
 <style type="text/css">
 p{
 	font-size: 16px;
